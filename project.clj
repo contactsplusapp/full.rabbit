2924923265ff6f00907fb7798815f57356ca113f
Mor Gazith <mor.gazith@benchmarkemail.com>
2023-03-12 10:27:36 +0200

    CON-1333 / CON-1564 / Remove ahead-of-time flag from all in-use FullContact libraries

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
-(defproject fullcontact/full.rabbit "0.10.7-SNAPSHOT"
+(defproject fullcontact/full.rabbit "0.10.7"
   :description "RabbitMQ sugar on top of langohr."
-  :url "https://github.com/fullcontact/full.rabbit"
+  :url "https://github.com/contactsplusapp/full.rabbit"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :repositories [["fullcontact" {:url "https://contactsplus.jfrog.io/artifactory/repo"}]
+                 ["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local" :sign-releases false}]
+                 ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local" :sign-releases false}]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.novemberain/langohr "3.3.0" :exclusions [cheshire]]
-                 [fullcontact/full.metrics "0.11.4"]
-                 [fullcontact/full.json "0.10.1"]
-                 [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.10.1" :exclusions [org.clojure/clojurescript]]]
+                 [fullcontact/full.metrics "0.13.1"]
+                 [fullcontact/full.json "0.10.3"]
+                 [fullcontact/full.async "1.1.1"]
+                 [fullcontact/full.core "1.1.3" :exclusions [org.clojure/clojurescript]]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -19,6 +21,5 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
-  :aot :all
   :plugins [[lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]}})
